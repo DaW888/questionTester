bb93f7607340389574d3e09a5bd0c110858c1e71
ddwajda <dawid888@mail.com>
2019-12-01 13:52:20 +0100

    style of login

--- login.php
+++ login.php
@@ -7,14 +7,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="author" content="Dawid Wajda">
     <meta name="description" content="3ic2">
-    <title>Strona Głowna</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="login.css">
+
+    <title>Logowanie</title>
 </head>
 
 <body>
-    <form action="./checkLogin.php" method="post">
+    <form action="./checkLogin.php" method="post" class="cont">
         <input type="text" name="name" placeholder="Podaj nazwe użytkownika" minlength="3" required>
         <input type="password" name="pass" placeholder="Podaj hasło" minlength="6" required>
-        <input type="submit" value="Submit">
+        <input type="submit" value="Zaloguj">
     </form>
 </body>
 
